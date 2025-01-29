fe583542cf0a82af8153f4ebf530a641eb0aeacc
Wilhelm_Carmevik <carmevik@kth.se>
2025-01-29 09:34:16 +0100

    Added to generate sample images

---
+++ sample_image_generator.py
@@ -0,0 +1,86 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import scipy
+import random
+
+def plot_heatmap(matrix, title='Heatmap', cmap='viridis', annot=False):
+    """
+    Plots a heatmap of the given matrix using only Matplotlib.
+    
+    Parameters:
+        matrix (ndarray): 2D NumPy array representing the matrix.
+        title (str): Title of the heatmap.
+        cmap (str): Colormap for the heatmap.
+        annot (bool): If True, display values in the heatmap.
+    """
+    plt.figure(figsize=(8, 6))
+    plt.imshow(matrix, cmap=cmap, aspect='auto')
+    plt.colorbar()
+    plt.title(title)
+    plt.xlabel("Columns")
+    plt.ylabel("Rows")
+    
+    if annot:
+        for i in range(matrix.shape[0]):
+            for j in range(matrix.shape[1]):
+                plt.text(j, i, f"{matrix[i, j]:.2f}", ha='center', va='center', color='black')
+    
+    plt.show()
+
+
+def plot_matrix(matrix1, matrix2, title="Heatmap"):
+    fig = plt.figure(figsize=(10,6))
+    gs = gridspec.GridSpec(1,2)
+
+    ax1 = fig.add_subplot(gs[0,0])
+    im = ax1.imshow(matrix1, cmap=None, interpolation='nearest')
+    fig.colorbar(im, ax=ax1)
+    ax1.set_title(title, fontsize=16)
+    ax1.tick_params(axis='both', which='both', length=0, labelbottom=False, labelleft=False)
+
+    ax2 = fig.add_subplot(gs[0,1])
+    im = ax2.imshow(matrix2, cmap=None, interpolation='nearest')
+    fig.colorbar(im, ax=ax2)
+    ax2.set_title(title, fontsize=16)
+    ax2.tick_params(axis='both', which='both', length=0, labelbottom=False, labelleft=False)
+
+    plt.tight_layout()
+    plt.savefig(title + ".png")
+    plt.show()
+
+
+
+def generate(xdim, ydim):
+
+    m = np.zeros((xdim, ydim))
+    m_shifted = np.zeros((xdim, ydim))
+
+    size = np.random.uniform(0.1, 0.2)
+    xpos = np.random.uniform(-0.2, 0.2)
+    ypos = np.random.uniform(-0.2, 0.2)
+    noise_level = 1
+
+    xshift = np.random.uniform(-0.1, 0.1)
+    yshift = np.random.uniform(-0.1, 0.1)
+    
+    for x in range(xdim):
+        xx = x / xdim - 1/2 
+        for y in range(ydim):
+            yy = y / ydim - 1/2
+            noise_1 = np.random.uniform(-noise_level, noise_level)
+            noise_2 = np.random.uniform(-noise_level, noise_level)
+            m[x,y] = 4*scipy.stats.norm.pdf(xx, xpos, size) * scipy.stats.norm.pdf(yy, ypos, size) + noise_1
+            m_shifted[x,y] = 4*scipy.stats.norm.pdf(xx, xpos + xshift, size) * scipy.stats.norm.pdf(yy, ypos + yshift, size) + noise_2
+    
+    return (m, m_shifted)
+
+if __name__ == "__main__":
+    #data = np.random.rand(10, 10)  # Generate a 10x10 random matrix
+    (a,b) = generate(50,50)
+    np.save("Matrix_1", a)
+    np.save("Matrix_1_shifted", b)
+
+    print("dasd")
+    plot_matrix(a, b)
+    #plot_heatmap(data, title='Random Matrix Heatmap', cmap='coolwarm', annot=True)
